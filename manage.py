2b9cfd05b43307426605232f34c5297b29cffee4
alex.garcia <a.garcia@eurodivisas.com>
2023-07-04 16:10:47 +0200

    Añado un comentario de prueba en manage.py

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+""añado un comentario""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
